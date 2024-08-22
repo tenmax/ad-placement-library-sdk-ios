cd410a39d12822d418c4b962f14b523f6a1cc71e
TenMax GitHub Action <tenmax-action@tenmax.com>
2024-08-22 08:55:52 +0000

    Update files for version v0.0.2

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TenMaxMobileAdsSDK",
-            url: "https://github.com/tenmax/ad-placement-library-sdk-ios/releases/download/v0.0.1/TenMaxMobileAdsSDK.xcframework.zip",
-            checksum: "293577999c02c49ad3cc5338530d5b684116605929ac4d5f8c1d0560b6a8219e"
+            url: "https://github.com/tenmax/ad-placement-library-sdk-ios/releases/download/v0.0.2/TenMaxMobileAdsSDK.xcframework.zip",
+            checksum: "b38460c91b625816bd2345dbb14a2998703b81acef9e73b20bedca95b794f0ae"
         ),
     ]
 )
